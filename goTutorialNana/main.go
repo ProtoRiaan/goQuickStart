5ce47b4a350cb70d1d72b97ea8ab153be6a9e57f
Riaan Schuld <rhschuld@gmail.com>
2024-07-28 14:34:54 -0400

    nana has some funky for logic

--- goTutorialNana/main.go
+++ goTutorialNana/main.go
@@ -1,38 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets = 50
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a totla of %v tickets and we still have %v tickets available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
-	// ask user for their name
+	for {
+		var userName string
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
+		// ask user for their name
 
-	fmt.Println("Enter your username:")
-	fmt.Scan(&userName)
-	fmt.Println("Enter your first name:")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your last name:")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
-	fmt.Println("Enter number of tickets:")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your username:")
+		fmt.Scan(&userName)
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+		fmt.Println("Enter number of tickets:")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Than you %v %v for booking %v tickets. You will receive confirmation at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("Than you %v %v for booking %v tickets. You will receive confirmation at %v\n", firstName, lastName, userTickets, email)
 
-	fmt.Println(&remainingTickets)
+		remainingTickets = remainingTickets - userTickets
+		fmt.Printf("We have %v tickets remaining\n", remainingTickets)
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T\n", conferenceTickets, remainingTickets)
-	fmt.Printf("User %v booked %v tickets for %v\n", userName, userTickets, conferenceName)
+		fmt.Printf("The whole Array : %v\n", bookings)
+		fmt.Printf("The first falue of the Array: %v\n", bookings[0])
+		fmt.Printf("Array type: %T\n", bookings[0])
+		fmt.Printf("Array length: %v\n", len(bookings))
+
+		fmt.Println(&remainingTickets)
+
+		fmt.Printf("conferenceTickets is %T, remainingTickets is %T\n", conferenceTickets, remainingTickets)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("User %v booked %v tickets for %v\n", firstNames, userTickets, conferenceName)
+	}
 }
