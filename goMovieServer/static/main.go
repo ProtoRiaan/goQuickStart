160d43e8f16282a03ddc32973765a1e3c6464402
Riaan Schuld <rhschuld@gmail.com>
2024-08-03 21:00:54 -0400

    added more create movie route to movie server exercise

--- goMovieServer/static/main.go
+++ goMovieServer/static/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -50,6 +52,16 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movies)
 }
 
 func main() {
