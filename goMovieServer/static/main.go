896d2853202635a5adf0536194346087b99d0765
Riaan Schuld <rhschuld@gmail.com>
2024-08-03 20:30:08 -0400

    started working on the first crud API

--- goMovieServer/static/main.go
+++ goMovieServer/static/main.go
@@ -1,20 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
 	Title    string    `json:"title"`
-	Director *Director `json:"director`
+	Director *Director `json:"director"`
 }
 
 type Director struct {
 	Firstname string `json:"firstname"`
-	lastname  string `json:"lastname"`
+	Lastname  string `json:"lastname"`
 }
 
 var movie []Movie
 
 func main() {
-	fileServer
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", delteMovies).Methods("DELETE")
+
+	fmt.Printf("Starting the server on port 8000\n")
+	log.Fatal(http.ListenAndServe(":8080,r"))
 
 }
