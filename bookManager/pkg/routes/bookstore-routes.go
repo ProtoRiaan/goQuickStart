2de80ca11703a3b675963a45dd39063693669b8e
Riaan Schuld <rhschuld@gmail.com>
2024-08-04 18:42:12 -0400

    trying to fix this stupid repo

--- bookManager/pkg/routes/bookstore-routes.go
+++ bookManager/pkg/routes/bookstore-routes.go
@@ -1,1 +1,17 @@
 package routes
+<<<<<<< HEAD
+=======
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/protoriaan/go-bookstore/pkg/controllers"
+)
+
+var RegisterBookstoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookByID).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBookBookByID).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBookBookByID).Methods("DELETE")
+}
+>>>>>>> df996b5 ( trying to fix this stupid repo)
