3c8e3d3023b024d259de0ff1bb0deab26b83c7c5
Riaan Schuld <rhschuld@gmail.com>
2024-08-03 20:52:35 -0400

    making some functions/routes for the movie server

--- goMovieServer/static/main.go
+++ goMovieServer/static/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -20,9 +21,44 @@ type Director struct {
 	Lastname  string `json:"lastname"`
 }
 
-var movie []Movie
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func delteMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...) // This syntax is deleteing the current movie in the index
+			break
+		}
+	}
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["ID"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
 
 func main() {
+	// Creating Some Movies
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "438228", Title: "Movie Two", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "3", Isbn: "438229", Title: "Movie Three", Director: &Director{Firstname: "Jane", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "4", Isbn: "438300", Title: "Movie Four", Director: &Director{Firstname: "Josh", Lastname: "Kelly"}})
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
